b2fdeec2bc3d34691ca65b983d00f86f70ec8a44
splunk-jessica <67291905+splunk-jessica@users.noreply.github.com>
2020-08-04 14:19:05 -0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,8 @@
 import sys
 from git import Repo
 
-repo = Repo('test')
-filename = 'test/helloworld.py'
-diff_output = repo.git.diff(filename)
-print(diff_output)
+f = open('README.md')
+print(f.readline())
 
 arg = sys.argv[1]
 print('hello world')

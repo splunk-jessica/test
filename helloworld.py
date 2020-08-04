f0e8cf708163289cc663c7a8c4a160ba3220bcef
splunk-jessica <67291905+splunk-jessica@users.noreply.github.com>
2020-08-04 12:16:45 -0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,11 @@
 import sys
+from createJira import *
+from git import Repo
+
+repo = Repo('splunk-jessica/test')
+filename = 'splunk-jessica/test/helloworld.py'
+diff_output = repo.git.diff(filename)
+print(diff_output)
 
 arg = sys.argv[1]
 print('hello world')

f49073795053b1d0d5cad7028d778ba138354a60
splunk-jessica <67291905+splunk-jessica@users.noreply.github.com>
2020-08-07 11:45:20 -0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,14 @@
 import sys
 from git import Repo
+from jira import JIRA
 
 f = open('README.md')
 print(f.readline())
 
-arg = sys.argv[1]
+if JIRA(sys.argv[1]):
+  print('can access secret')
+else:
+  print('cant access secret')
 print('hello world')
 if arg:
-  print('can access secret')
+  
